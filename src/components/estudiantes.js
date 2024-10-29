0e1c2299e26ab4e4e95fc0523549cdcef978cf1e
Joseligos <joseligo14@gmail.com>
2024-10-28 21:08:45 -0500

    v4

--- src/components/estudiantes.js
+++ src/components/estudiantes.js
@@ -2,21 +2,23 @@ import React from 'react';
 import './estyles.css';
 function Estudiantes() {
   return (
-    <div style={styles.body}>
-        <div className="container">
-        <input type="text" className="search-bar" placeholder="Busqueda" />
-        <h1>Estudiantes</h1>
-        <div className="students-background"></div>
-        <div className="students-list">
-            <StudentCard name="Castro Salinas Miguel Angel" gender="boy" />
-            <StudentCard name="Delgado Vargas Paula Andrea" gender="girl" />
-            <StudentCard name="Gomez Fernandez Ana Maria" gender="girl" />
-            <StudentCard name="Hernandez Garcia Jose" gender="boy" />
-            <StudentCard name="Ortiz Mendoza Laura Isabel" gender="girl" />
-            <StudentCard name="Perez Ortiz Juan Carlos" gender="boy" />
-            <StudentCard name="Torres Gutierrez Maria Jose" gender="girl" />
-        </div>
-        <button className="view-all-button">Ver todos</button>
+    <div className='origen'>
+        <div className='body'>
+            <div className="container">
+            <input type="text" className="search-bar" placeholder="Busqueda" />
+            <h1>Estudiantes</h1>
+            <div className="students-background"></div>
+            <div className="students-list">
+                <StudentCard name="Castro Salinas Miguel Angel" gender="boy" />
+                <StudentCard name="Delgado Vargas Paula Andrea" gender="girl" />
+                <StudentCard name="Gomez Fernandez Ana Maria" gender="girl" />
+                <StudentCard name="Hernandez Garcia Jose" gender="boy" />
+                <StudentCard name="Ortiz Mendoza Laura Isabel" gender="girl" />
+                <StudentCard name="Perez Ortiz Juan Carlos" gender="boy" />
+                <StudentCard name="Torres Gutierrez Maria Jose" gender="girl" />
+            </div>
+            <button className="view-all-button">Ver todos</button>
+            </div>
         </div>
     </div>
   );
